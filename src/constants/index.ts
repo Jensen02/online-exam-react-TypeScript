b989cb9c616ce4b145e05dac16c541229da95c3c
zhaosongsong <1739416503@qq.com>
2020-04-09 23:48:01 +0800

    增添常量

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Description: 常量
+ * @GitHub: https://github.com/Jensen02
+ * @Author: Jensen
+ * @Date: 2020-04-09 21:44:49
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-04-09 21:57:44
+ */
+ export const SET_USER_INFO = 'SET_USER_INFO';
+ export const SET_THEME = 'SET_THEME';
+ export const SET_USER_ROLE = 'SET_USER_ROLE';
