37d0b81fb3fb3ed1d84773f79c00fb6c03dafc5c
zhaosongsong <1739416503@qq.com>
2020-04-09 23:42:54 +0800

    删除注释代码

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Layout, Breadcrumb } from 'antd';
-// import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 import HeaderComponent from '../Header/Header';
 import SiderComponent from '../Sider/Sider';
 import { MenuProp } from '../Menu/types';
 import './Layout.less';
 
-// const { SubMenu } = Menu;
 const { Content } = Layout;
 
 const LayoutComponent: React.FC<MenuProp> = ({ menuArray }) => {
@@ -15,67 +13,13 @@ const LayoutComponent: React.FC<MenuProp> = ({ menuArray }) => {
       <HeaderComponent />
       <Layout>
         <SiderComponent menuArray={menuArray} />
-        {/* <Sider
+        <Layout
           style={{
-            width: '200px',
+            padding: '0 24px 24px',
             overflowY: 'auto',
-            height: '100vh',
+            height: '100vh'
           }}
-          className="site-layout-background"
         >
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{ height: '100%', borderRight: 0 }}
-          >
-            <SubMenu
-              key="sub1"
-              title={
-                <span>
-                  <UserOutlined />
-                  subnav 1
-                </span>
-              }
-            >
-              <Menu.Item key="1">option1</Menu.Item>
-              <Menu.Item key="2">option2</Menu.Item>
-              <Menu.Item key="3">option3</Menu.Item>
-              <Menu.Item key="4">option4</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={
-                <span>
-                  <LaptopOutlined />
-                  subnav 2
-                </span>
-              }
-            >
-              <Menu.Item key="5">option5</Menu.Item>
-              <Menu.Item key="6">option6</Menu.Item>
-              <Menu.Item key="7">option7</Menu.Item>
-              <Menu.Item key="8">option8</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub3"
-              title={
-                <span>
-                  <NotificationOutlined />
-                  subnav 3
-                </span>
-              }
-            >
-              <Menu.Item key="9">option9</Menu.Item>
-              <Menu.Item key="10">option10</Menu.Item>
-              <Menu.Item key="11">option11</Menu.Item>
-              <Menu.Item key="12">option12</Menu.Item>
-            </SubMenu>
-          </Menu>
-        </Sider> */}
-        <Layout style={{ padding: '0 24px 24px',
-            overflowY: 'auto',
-            height: '100vh', }}>
           <Breadcrumb style={{ margin: '16px 0', textAlign: 'left' }}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
             <Breadcrumb.Item>List</Breadcrumb.Item>
