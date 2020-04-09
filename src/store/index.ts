c342ea8618b7bc66a0e694042814c1459a1d219a
zhaosongsong <1739416503@qq.com>
2020-04-09 23:45:07 +0800

    引入reducer和中间件，生成store并导出

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,11 +4,15 @@
  * @Author: Jensen
  * @Date: 2020-03-26 23:36:43
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-03-26 23:46:29
+ * @LastEditTime: 2020-04-09 23:31:21
  */
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import rootReducer from '../reducers';
 
 const loggerMiddleware = createLogger();
 
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware, thunkMiddleware));
+
+export default store;
