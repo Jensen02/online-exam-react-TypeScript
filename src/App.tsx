fd438da32db0acc6f241ea547ab02cce073503e8
zhaosongsong <1739416503@qq.com>
2020-04-09 23:44:05 +0800

    引入redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import LayoutComponent from './components/Layout/Layout';
+import './App.css';
 
 function App() {
   const menuArray = [
@@ -54,21 +54,13 @@ function App() {
   ];
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <LayoutComponent menuArray={menuArray} />
+      <LayoutComponent menuArray={menuArray}>
+        <Router>
+          <Switch>
+            <Redirect from='/' to='/login' />
+          </Switch>
+        </Router>
+      </LayoutComponent>
     </div>
   );
 }
