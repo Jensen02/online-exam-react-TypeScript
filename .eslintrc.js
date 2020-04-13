4050372c90e949df06953413f51cb87a22d29f06
zhaosongsong <1739416503@qq.com>
2020-04-13 14:29:51 +0800

    增加.eslintrc.js文件

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+/*
+ * @Description: mo
+ * @GitHub: https://github.com/Jensen02
+ * @Author: Jensen
+ * @Date: 2020-04-13 12:47:30
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-04-13 12:50:05
+ */
+module.exports = {
+  "plugins": [
+    // ...
+    "react-hooks"
+  ],
+  "rules": {
+    // ...
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  }
+}
