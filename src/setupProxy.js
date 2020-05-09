441c8e3d7b87919e6564e38eab7906e2666dd2bc
Jensen02 <Jensen21@163.com>
2020-05-10 00:12:47 +0800

    开启代理，删除项目相对路径字段配置

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,23 +4,23 @@
  * @Author: Jensen
  * @Date: 2020-04-16 22:22:28
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-05-09 15:52:17
+ * @LastEditTime: 2020-05-09 18:42:14
  */
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  // app.use(createProxyMiddleware(
-  //   '/api/v1', {
-  //   target: 'http://101.133.211.70/',
-  //   changeOrigin: true
-  // },
-  //   '/area', {
-  //   target: 'https://api.jisuapi.com/',
-  //   changeOrigin: true
-  // },
-  //   '/apis', {
-  //   target: 'http://api.yonyoucloud.com/',
-  //   changeOrigin: true
-  // }
-  // ));
+  app.use(createProxyMiddleware(
+    '/api/v1', {
+    target: 'http://101.133.211.70/',
+    changeOrigin: true
+  },
+    '/area', {
+    target: 'https://api.jisuapi.com/',
+    changeOrigin: true
+  },
+    '/apis', {
+    target: 'http://api.yonyoucloud.com/',
+    changeOrigin: true
+  }
+  ));
 };
